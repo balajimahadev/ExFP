c752e4fe5af75790338ac073540de1eb75dec1ee
Balaji Mahadev <mahadev.balaji@gmail.com>
2021-12-25 23:52:34 -0600

    Update ex14-23.py

--- ex14-23.py
+++ ex14-23.py
@@ -55,3 +55,28 @@ def password_valid():
 
 
 password_valid()
+
+
+
+# Ex Number to names
+def number_to_names():
+
+    number = input("Please enter the number of the month: ")
+
+    return {
+        '1': "January",
+        '2': "February",
+        '3': "March",
+        '4': "April",
+        '5': "May",
+        '6': "June",
+        '7': "July",
+        '8': "August",
+        '9': "September",
+        '10': "October",
+        '11': "November",
+        '12': "December"
+    }[number]
+
+
+print(number_to_names())
